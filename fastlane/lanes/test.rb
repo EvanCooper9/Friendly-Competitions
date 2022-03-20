8ce62fb31b58250214a67899894f6de08bbed28d
Evan Cooper <evan.cooper@shoebox.md>
2022-03-20 13:59:33 -0400

    Cache xcode build for unit tests (#31)

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -2,6 +2,7 @@ lane :test do
     run_tests(
         clean: !is_ci,
         scheme: "Friendly Competitions",
-        device: "iPhone 13 Pro Max"
+        device: "iPhone 13 Pro Max",
+        xcodebuild_command: "xcodebuild CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++"
     )
 end
