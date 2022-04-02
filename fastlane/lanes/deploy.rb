36001ca2c4e3130dcf0e2522f9f41024ceebecea
Evan Cooper <evan.cooper@rogers.com>
2022-04-01 23:33:03 -0400

    use certificates lane

--- fastlane/lanes/deploy.rb
+++ fastlane/lanes/deploy.rb
@@ -7,11 +7,7 @@ lane :deploy do
         duration: 1200, # optional (maximum 1200)
         in_house: false # optional but may be required if using match/sigh
     ) if is_ci
-    match(
-        type: "appstore", 
-        app_identifier: "com.evancooper.FriendlyCompetitions",
-        readonly: true
-    )
+    certificates
     increment_build_number(
         build_number: latest_testflight_build_number + 1
     )
