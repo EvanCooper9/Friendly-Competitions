4ff92424b8fcbc8325d2fc20c4e664b2f3828ba3
Evan Cooper <evan.cooper@rogers.com>
2022-04-13 18:48:08 -0400

    use ssh key

--- fastlane/lanes/certificates.rb
+++ fastlane/lanes/certificates.rb
@@ -1,4 +1,9 @@
 lane :certificates do
-    match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug")
-    match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions")
+    if is_ci
+        match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug", git_private_key: ENV["GIT_PRIVATE_KEY"])
+        match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions", git_private_key: ENV["GIT_PRIVATE_KEY"])
+    else
+        match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug")
+        match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions")
+    end
 end
