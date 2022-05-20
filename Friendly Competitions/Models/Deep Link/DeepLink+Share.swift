86d1ba08eac6236f62afc3d207efd760d5b76377
Evan Cooper <evan.cooper@rogers.com>
2022-05-19 20:17:08 -0400

    Add invite link sharing to profile

---
+++ Friendly Competitions/Models/Deep Link/DeepLink+Share.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+extension DeepLink {
+    func share() {
+        let activityVC = UIActivityViewController(
+            activityItems: itemsForSharing,
+            applicationActivities: nil
+        )
+        activityVC.excludedActivityTypes = [.mail, .addToReadingList, .assignToContact, .markupAsPDF, .openInIBooks, .saveToCameraRoll, .print]
+
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .compactMap { $0 as? UIWindowScene }
+            .first?
+            .windows
+            .filter(\.isKeyWindow)
+            .first
+
+        keyWindow?.rootViewController?
+            .topViewController
+            .present(activityVC, animated: true, completion: nil)
+    }
+}
