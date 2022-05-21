8a0855015007b694e28dff34df81a419f237636f
Evan Cooper <evan.cooper@rogers.com>
2022-05-21 13:45:44 -0400

    Apple review information (#54)

--- fastlane/lanes/deploy.rb
+++ fastlane/lanes/deploy.rb
@@ -1,5 +1,6 @@
 lane :deploy do
     setup_ci
+    
     app_store_connect_api_key(
         key_id: ENV["APP_STORE_CONNECT_API_KEY_ID"],
         issuer_id: ENV["APP_STORE_CONNECT_API_ISSUER_ID"],
@@ -7,24 +8,45 @@ lane :deploy do
         duration: 1200, # optional (maximum 1200)
         in_house: false # optional but may be required if using match/sigh
     ) if is_ci
+    
     certificates
+
+    # Current Version (via xcodeproj)
+    current_version = get_version_number
+    testflight_build = latest_testflight_build_number
+    testflight_version = Actions.lane_context[SharedValues::LATEST_TESTFLIGHT_VERSION]
+    new_build = testflight_version == current_version ? testflight_build + 1 : 1
+    
     increment_build_number(
-        build_number: latest_testflight_build_number + 1
+        build_number: new_build
     )
+
     build_app(
         include_bitcode: true
     )
-    upload_to_app_store(
-        force: true,
-        precheck_include_in_app_purchases: false
+
+    upload_to_testflight(
+        notify_external_testers: false
     )
+
     download_dsyms(
         version: "latest",
         wait_for_dsym_processing: true,
         wait_timeout: 900 # 15 min
     )
+
     upload_symbols_to_crashlytics(
         gsp_path: "Friendly Competitions/Firebase/Release/GoogleService-Info.plist",
         binary_path: "fastlane/scripts/upload-symbols"
+    )
+
+    upload_to_app_store(
+        force: true,
+        submit_for_review: true,
+        precheck_include_in_app_purchases: false,
+        submission_information: {
+            add_id_info_uses_idfa: false
+        },
+        skip_binary_upload: true
     )
 end
