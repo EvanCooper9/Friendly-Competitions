1459f90fad1eb087b59811aa7789465c9f4129a0
Evan Cooper <evan.cooper@rogers.com>
2022-09-18 19:36:51 -0400

    remove custom xcode build

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -1,8 +1,3 @@
 lane :test do
-    run_tests(
-        clean: !is_ci,
-        scheme: "Friendly Competitions",
-        device: "iPhone 13 Pro Max",
-        xcodebuild_command: "xcodebuild CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++",
-    )
+    run_tests
 end
