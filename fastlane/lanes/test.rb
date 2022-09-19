18d7f678938fe1589730c5e139f9c3d1b1539678
Evan Cooper <evan.cooper@rogers.com>
2022-09-19 12:07:28 -0400

    Add xcode build cache

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -1,3 +1,8 @@
 lane :test do
-    run_tests
+    run_tests(
+        clean: !is_ci,
+        scheme: "Friendly Competitions",
+        device: "iPhone 13 Pro Max",
+        xcodebuild_command: "xcodebuild CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++"
+    )
 end
