77688d8bd1ab081006e54752b1e951841e90ff16
Evan Cooper <evan.cooper@rogers.com>
2022-09-19 10:55:52 -0400

    Run tests through fastlane again

--- Friendly Competitions/Views/ViewModels+Registration.swift
+++ Friendly Competitions/Views/ViewModels+Registration.swift
@@ -1,17 +1,18 @@
 import Resolver
+import ResolverAutoregistration
 
 extension Resolver {
     static func registerViewModels() {
-        register { (_, args) in CompetitionViewModel(competitionsManager: resolve(), userManager: resolve(), competition: args()) }
-        register { DashboardViewModel(activitySummaryManager: resolve(), competitionsManager: resolve(), friendsManager: resolve(), permissionsManager: resolve(), userManager: resolve()) }
-        register { ExploreViewModel(competitionsManager: resolve()) }
-        register { HomeViewModel(competitionsManager: resolve(), friendsManager: resolve()) }
-        register { (_, args) in InviteFriendsViewModel(competitionsManager: resolve(), friendsManager: resolve(), userManager: resolve(), action: args())}
-        register { NewCompetitionViewModel(competitionsManager: resolve(), friendsManager: resolve(), userManager: resolve()) }
-        register { PermissionsViewModel(permissionsManager: resolve()) }
-        register { ProfileViewModel(authenticationManager: resolve(), userManager: resolve()) }
-        register { SignInViewModel(appState: resolve(), authenticationManager: resolve()) }
-        register { (_, args) in UserViewModel(friendsManager: resolve(), userManager: resolve(), user: args()) }
-        register { VerifyEmailViewModel(appState: resolve(), authenticationManager: resolve(), userManager: resolve()) }
+        autoregister(CompetitionViewModel.self, argument: Competition.self, initializer: CompetitionViewModel.init)
+        autoregister(DashboardViewModel.self, initializer: DashboardViewModel.init)
+        autoregister(ExploreViewModel.self, initializer: ExploreViewModel.init)
+        autoregister(HomeViewModel.self, initializer: HomeViewModel.init)
+        autoregister(InviteFriendsViewModel.self, argument: InviteFriendsAction.self, initializer: InviteFriendsViewModel.init)
+        autoregister(NewCompetitionViewModel.self, initializer: NewCompetitionViewModel.init)
+        autoregister(PermissionsViewModel.self, initializer: PermissionsViewModel.init)
+        autoregister(ProfileViewModel.self, initializer: ProfileViewModel.init)
+        autoregister(SignInViewModel.self, initializer: SignInViewModel.init)
+        autoregister(UserViewModel.self, argument: User.self, initializer: UserViewModel.init)
+        autoregister(VerifyEmailViewModel.self, initializer: VerifyEmailViewModel.init)
     }
 }
