b5c99491c7cfa7f13f098842996429c31bad32b8
Evan Cooper <evan.cooper@rogers.com>
2023-01-04 18:33:11 -0500

    update device for real

--- Friendly Competitions/Views/Competitions/History/CompetitionHistoryView.swift
+++ Friendly Competitions/Views/Competitions/History/CompetitionHistoryView.swift
@@ -86,10 +86,3 @@ struct CompetitionHistoryView_Previews: PreviewProvider {
     }
 }
 #endif
-
-
-struct VisualEffectView: UIViewRepresentable {
-    var effect: UIVisualEffect?
-    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
-    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
-}
