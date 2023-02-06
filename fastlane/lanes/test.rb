c3a36ea282dae559794aa2b5af61aee44753d541
Evan Cooper <evan.cooper@rogers.com>
2023-02-06 12:34:35 -0500

    action instead of workflow

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -1,6 +1,8 @@
 lane :test do
     run_tests(
-        skip_build: true,
+        clean: !is_ci,
+        scheme: "Friendly Competitions",
         device: "iPhone 14 Pro Max",
+        xcargs: "CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++"
     )
 end
