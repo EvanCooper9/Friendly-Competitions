05fd13625fa40f6b61b91145a8c38075a6727eb6
Evan Cooper <evan.cooper@rogers.com>
2023-03-21 19:09:29 -0400

    Build Firebase from source to prevent preview crash

--- Friendly Competitions/API/API+Firebase.swift
+++ Friendly Competitions/API/API+Firebase.swift
@@ -1,6 +1,7 @@
 import Combine
 import CombineExt
 import FirebaseFunctions
+import FirebaseFunctionsCombineSwift
 
 extension Functions: API {
     func call(_ endpoint: String, with data: [String : Any]?) -> AnyPublisher<Void, Error> {
