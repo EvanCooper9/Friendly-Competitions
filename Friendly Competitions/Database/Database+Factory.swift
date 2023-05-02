e3023696e96722ba414786d6806323baf99370a9
Evan Cooper <evan.cooper@rogers.com>
2023-05-02 11:53:53 -0400

    Add default database source to fetch from server first then cache

--- Friendly Competitions/Database/Database+Factory.swift
+++ Friendly Competitions/Database/Database+Factory.swift
@@ -12,11 +12,11 @@ extension Container {
             case .prod:
                 break
             case .debugLocal:
-                settings.isPersistenceEnabled = false
+                settings.cacheSettings = MemoryCacheSettings()
                 settings.isSSLEnabled = false
                 settings.host = "localhost:\(8080)"
             case .debugRemote(let destination):
-                settings.isPersistenceEnabled = false
+                settings.cacheSettings = MemoryCacheSettings()
                 settings.isSSLEnabled = false
                 settings.host = "\(destination):\(8080)"
             }
