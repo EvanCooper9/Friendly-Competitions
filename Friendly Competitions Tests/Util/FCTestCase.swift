220b333dd4788c72afa9a6ef6ca6643b724bc000
Evan Cooper <evan.cooper@rogers.com>
2023-07-14 09:56:12 -0400

    fix build

--- Friendly Competitions Tests/Util/FCTestCase.swift
+++ Friendly Competitions Tests/Util/FCTestCase.swift
@@ -37,43 +37,38 @@ class FCTestCase: XCTestCase {
 
     var cancellables = Cancellables()
 
-    private var _container = Container()
-    var container: Container { _container }
-
     override func setUp() {
         super.setUp()
-        _container = .init()
-        Container.shared = _container
         register()
     }
 
     private func register() {
-        container.activitySummaryCache.register { self.activitySummaryCache }
-        container.activitySummaryManager.register { self.activitySummaryManager }
-        container.analyticsManager.register { self.analyticsManager }
-        container.api.register { self.api }
-        container.appServices.register { self.appServices }
-        container.appState.register { self.appState }
-        container.auth.register { self.auth }
-        container.authenticationCache.register { self.authenticationCache }
-        container.authenticationManager.register { self.authenticationManager }
-        container.competitionCache.register { self.competitionCache }
-        container.competitionsManager.register { self.competitionsManager }
-        container.database.register { self.database }
-        container.environmentCache.register { self.environmentCache }
-        container.environmentManager.register { self.environmentManager }
-        container.friendsManager.register { self.friendsManager }
-        container.healthKitManager.register { self.healthKitManager }
-        container.healthStore.register { self.healthStore }
-        container.notificationsManager.register { self.notificationsManager }
-        container.premiumManager.register { self.premiumManager }
-        container.scheduler.register { self.scheduler.eraseToAnyScheduler() }
-        container.searchClient.register { self.searchClient }
-        container.searchManager.register { self.searchManager }
-        container.stepCountManager.register { self.stepCountManager }
-        container.signInWithAppleProvider.register { self.signInWithAppleProvider }
-        container.storageManager.register { self.storageManager }
-        container.userManager.register { self.userManager }
-        container.workoutManager.register { self.workoutManager }
+        Container.shared.activitySummaryCache.register { self.activitySummaryCache }
+        Container.shared.activitySummaryManager.register { self.activitySummaryManager }
+        Container.shared.analyticsManager.register { self.analyticsManager }
+        Container.shared.api.register { self.api }
+        Container.shared.appServices.register { self.appServices }
+        Container.shared.appState.register { self.appState }
+        Container.shared.auth.register { self.auth }
+        Container.shared.authenticationCache.register { self.authenticationCache }
+        Container.shared.authenticationManager.register { self.authenticationManager }
+        Container.shared.competitionCache.register { self.competitionCache }
+        Container.shared.competitionsManager.register { self.competitionsManager }
+        Container.shared.database.register { self.database }
+        Container.shared.environmentCache.register { self.environmentCache }
+        Container.shared.environmentManager.register { self.environmentManager }
+        Container.shared.friendsManager.register { self.friendsManager }
+        Container.shared.healthKitManager.register { self.healthKitManager }
+        Container.shared.healthStore.register { self.healthStore }
+        Container.shared.notificationsManager.register { self.notificationsManager }
+        Container.shared.premiumManager.register { self.premiumManager }
+        Container.shared.scheduler.register { self.scheduler.eraseToAnyScheduler() }
+        Container.shared.searchClient.register { self.searchClient }
+        Container.shared.searchManager.register { self.searchManager }
+        Container.shared.stepCountManager.register { self.stepCountManager }
+        Container.shared.signInWithAppleProvider.register { self.signInWithAppleProvider }
+        Container.shared.storageManager.register { self.storageManager }
+        Container.shared.userManager.register { self.userManager }
+        Container.shared.workoutManager.register { self.workoutManager }
     }
 }
