13d602aa7869a2ff624ea027d6be848b7e7ea487
Evan Cooper <evan.cooper@rogers.com>
2023-08-09 08:43:30 -0400

    Cache SPM

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -4,5 +4,6 @@ lane :test do
         scheme: "Friendly Competitions",
         device: "iPhone 14 Pro Max",
         xcargs: "CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++",
+        cloned_source_packages_path: "SourcePackages"
     )
 end
