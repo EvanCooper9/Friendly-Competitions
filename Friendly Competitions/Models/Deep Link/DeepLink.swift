70e5b976322dfc5a0599cf1d7ecf865d2f43132e
Evan Cooper <evan.cooper@rogers.com>
2023-10-20 00:17:29 -0400

    Home screen banners

--- Friendly Competitions/Models/Deep Link/DeepLink.swift
+++ Friendly Competitions/Models/Deep Link/DeepLink.swift
@@ -5,6 +5,11 @@ import Foundation
 
 enum DeepLink: Equatable {
 
+    private final class DeepLinkDependency {
+        @Injected(\.competitionsManager) private var competitionsManager
+        @Injected(\.friendsManager) private var friendsManager
+    }
+
     private enum Constants {
         static let baseURL = URL(string: "https://friendly-competitions.app")!
         static let user = "user"
