d0c06b297d58b32f331d2f52b383b4e29fd31929
Evan Cooper <evan.cooper@rogers.com>
2024-02-14 17:49:46 -0500

    fix test lane

--- fastlane/lanes/test.rb
+++ fastlane/lanes/test.rb
@@ -1,7 +1,7 @@
 lane :test do
     run_tests(
         clean: false,
-        scheme: "Friendly Competitions",
+        scheme: "FriendlyCompetitions",
         device: "iPhone 14 Pro Max",
         xcargs: "CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++",
     )
