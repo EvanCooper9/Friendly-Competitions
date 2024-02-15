a46eb6399a3a6b5a3d22d8603a1e36855ead5e7f
Evan Cooper <evan.cooper@uber.com>
2024-02-15 14:28:57 -0500

    oops

--- FriendlyCompetitions/Managers/WidgetData/WidgetDataManaging+Factory.swift
+++ FriendlyCompetitions/Managers/WidgetData/WidgetDataManaging+Factory.swift
@@ -1,6 +1,5 @@
 import Factory
 
-@available(iOS, introduced: 17)
 extension Container {
     var widgetDataManager: Factory<WidgetDataManaging> {
         self { WidgetDataManager() }.scope(.shared)
