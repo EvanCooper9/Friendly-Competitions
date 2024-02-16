02a029a46e25a3255f284eb4b2df3f1254702659
Evan Cooper <evan.cooper@rogers.com>
2024-02-15 21:18:22 -0500

    run match for widgets

--- fastlane/lanes/certificates.rb
+++ fastlane/lanes/certificates.rb
@@ -1,5 +1,7 @@
 lane :certificates do
-    match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug")
     match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions")
+    match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug")
+    match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug.FriendlyCompetitionsWidgets")
     match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions")
+    match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions.FriendlyCompetitionsWidgets")
 end
