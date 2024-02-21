e16794d5d25174a5df1a29c535904f6bf5a0a9a3
Evan Cooper <evan.cooper@rogers.com>
2024-02-21 14:38:14 -0500

    run fastlane certificates

--- fastlane/lanes/certificates.rb
+++ fastlane/lanes/certificates.rb
@@ -1,7 +1,13 @@
 lane :certificates do
+    # development release identifier
     match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions")
+    match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.FriendlyCompetitionsWidgets")
+
+    # development debug identifier
     match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug")
     match(type: "development", app_identifier: "com.evancooper.FriendlyCompetitions.debug.FriendlyCompetitionsWidgets")
+    
+    # app store release identifier
     match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions")
     match(type: "appstore", app_identifier: "com.evancooper.FriendlyCompetitions.FriendlyCompetitionsWidgets")
 end
